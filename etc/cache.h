f307b4bad8dd8811b830d95d87b8024950364636
TUNBudi06 <70352065+TUNBudi06@users.noreply.github.com>
2021-08-05 21:16:34 +0700

    Add files via upload

---
+++ etc/cache.h
@@ -0,0 +1,28 @@
+#!/bin/bash
+tx=$HOME/thepakv4
+
+cd $tx
+ source etc/folder.sh
+
+    cd $tx
+    if [[ -d 'SU' ]]
+     then 
+      rm -rf SU
+      echo 'cache clear'
+    else
+      echo 'cache clear'
+    fi
+    
+    cd $down
+    echo 'removing output cache'
+    if [[ -d 'output' ]]
+     then
+       rm -rf output
+       echo 'success'
+    else
+       echo 'success'
+    fi
+    
+    
+    cd $tx
+    rm -rf cache
---
+++ etc/folder.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+QBMSDIR=$PREFIX/share/quickbms
+tx=$HOME/thepakv4
+down=/storage/emulated/0/Download
+dpak=/storage/emulated/0/Download/pak
+opak=/storage/emulated/0/Download/output
